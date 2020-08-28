adcc21dd4753a5f360e2ccca549d6fee132d18fd
analubarreto <analuibm@gmail.com>
2020-08-28 10:22:27 -0300

    started profile page

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,6 +1,25 @@
 <template>
   <q-page class="page">
-    <p>Profile Page</p>
+    <main class="row absolute-center">
+      <div class="column">
+        <div class="image" />
+      </div>
+      <div class="column">
+        <p class="user-data">Lucifer Morning Star</p>
+        <p class="user-data">lucifermorningstart@gmail.com</p>
+        <span class="user-data">
+          <p
+            class="wishlist"
+            clickable
+          >
+            <q-icon
+              name="grade"
+              style="color: #6C7194; font-size: 1.4em;"
+            />
+            Wishlist</p>
+        </span>
+      </div>
+    </main>
   </q-page>
 </template>
 
@@ -13,5 +32,24 @@ export default {
 <style lang="scss">
 .page {
   background-color: $secondary;
+  .image {
+    width: 500px;
+    height: 500px;
+    border-radius: 15%;
+    color: #000;
+  }
+
+  .user-data {
+    font-size: 25px;
+
+    .wishlist {
+      color: #6c7194;
+
+      &:hover {
+        color: #939bcc;
+        cursor: pointer;
+      }
+    }
+  }
 }
 </style>
